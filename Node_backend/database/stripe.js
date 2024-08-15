a77136addf606dffa8449dab95c8f0dfbe37e21c
top329 <tanakayuki329@outlook.com>
2024-08-16 02:31:57 +1000

    fix: stripe key

--- Node_backend/database/stripe.js
+++ Node_backend/database/stripe.js
@@ -1,4 +1,4 @@
 // stripe.js
-const stripe = require('stripe')(process.env.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);
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 module.exports = stripe;
